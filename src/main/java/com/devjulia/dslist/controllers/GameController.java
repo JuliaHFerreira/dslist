f020b2eec4c057263eab391bd737e870e7e7c758
JuliaHFerreira <juliahelena317@gmail.com>
2025-08-20 20:51:25 -0300

    feat: GET game by id

--- src/main/java/com/devjulia/dslist/controllers/GameController.java
+++ src/main/java/com/devjulia/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devjulia.dslist.controllers;
 
+import com.devjulia.dslist.dto.GameDTO;
 import com.devjulia.dslist.dto.GameMinDTO;
 import com.devjulia.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) { // @PathVariable serve para capturar valores da URL e passá-los como parâmetros
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- src/main/java/com/devjulia/dslist/services/GameService.java
+++ src/main/java/com/devjulia/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devjulia.dslist.services;
 
+import com.devjulia.dslist.dto.GameDTO;
 import com.devjulia.dslist.dto.GameMinDTO;
 import com.devjulia.dslist.entities.Game;
 import com.devjulia.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired //injetando
     private GameRepository gameRepository;
 
+    @Transactional (readOnly = true) //Fica transacional ACID -> Ele garante que o dado chegue inteiro na base | (ReadOnly = true) diz que a operação vai ser apenas leitura fazendo ser mais rapido
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        //fazer tratamento de exceções
+        return new GameDTO(result);
+    }
+    @Transactional (readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         // List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); -> pode ser feito assim
